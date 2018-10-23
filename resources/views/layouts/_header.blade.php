0411f51fb158df12e35dfbf349be9df163c7978f
egoeyre <egoeyre@qq.com>
2018-10-23 22:02:19 +0800

    Basic user profile editing

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -36,10 +36,15 @@
 
                                 <ul class="dropdown-menu">
                                     <li>
+                                        <a href="{{ route('users.edit', Auth::id()) }}">
+                                             编辑资料
+                                        </a>
+                                    </li>
+                                    <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            退出登录
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
