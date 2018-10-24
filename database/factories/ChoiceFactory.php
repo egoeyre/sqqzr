dbf911f2cbf9c6c10c445afc68b3d7596be01d50
egoeyre <egoeyre@qq.com>
2018-10-24 15:28:32 +0800

    Seeding choices and users data

--- database/factories/ChoiceFactory.php
+++ database/factories/ChoiceFactory.php
@@ -3,7 +3,21 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Choice::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+    $op = ['A','B','C','D'];
+    $updated_at = $faker->dateTimeThisMonth();
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        //'user_id' = $sentence
+        //'category_id'
+        'title'		=> $sentence,
+        'optiona'	=> $faker->word(),
+        'optionb'	=> $faker->word(),
+        'optionc'	=> $faker->word(),
+        'optiond'	=> $faker->word(),
+        'answer'	=> $faker->randomElement($op),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
--- database/seeds/ChoicesTableSeeder.php
+++ database/seeds/ChoicesTableSeeder.php
@@ -2,17 +2,36 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Choice;
+use App\Models\User;
+use App\Models\Category;
 
 class ChoicesTableSeeder extends Seeder
 {
     public function run()
     {
-        $choices = factory(Choice::class)->times(50)->make()->each(function ($choice, $index) {
-            if ($index == 0) {
-                // $choice->field = 'value';
-            }
+       // 所有用户 ID 数组，如：[1,2,3,4]
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        // 所有分类 ID 数组，如：[1,2,3,4]
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+         // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        $choices = factory(Choice::class)
+        			->times(100)
+        			->make()
+        			->each(function ($choice, $index)
+                        use ($user_ids, $category_ids, $faker)
+        {
+            // 从用户 ID 数组中随机取出一个并赋值
+            $choice->user_id = $faker->randomElement($user_ids);
+
+            // 话题分类，同上
+            $choice->category_id = $faker->randomElement($category_ids);
         });
 
+        // 将数据集合转换为数组，并插入到数据库中
         Choice::insert($choices->toArray());
     }
 
