aac23c089990fdc439a24af9d8318ec946e5ac96
egoeyre <egoeyre@qq.com>
2018-10-27 10:54:40 +0800

    Seeding blanks data

--- database/factories/BlankFactory.php
+++ database/factories/BlankFactory.php
@@ -3,7 +3,15 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Blank::class, function (Faker $faker) {
+    $num = [1,2,3,4];
+    $updated_at = $faker->dateTimeThisMonth();
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title'		=> $faker->sentence(),
+        'anwser'	=> $faker->word(),
+        'amount'	=> $faker->randomElement($num),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
--- database/seeds/BlanksTableSeeder.php
+++ database/seeds/BlanksTableSeeder.php
@@ -2,18 +2,38 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Blank;
+use App\Models\User;
+use App\Models\Category;
+
 
 class BlanksTableSeeder extends Seeder
 {
     public function run()
     {
-        $blanks = factory(Blank::class)->times(50)->make()->each(function ($blank, $index) {
-            if ($index == 0) {
-                // $blank->field = 'value';
-            }
+        // 所有用户 ID 数组，如：[1,2,3,4]
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        // 所有分类 ID 数组，如：[1,2,3,4]
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+         // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        $choices = factory(Blank::class)
+        			->times(100)
+        			->make()
+        			->each(function ($blank, $index)
+                        use ($user_ids, $category_ids, $faker)
+        {
+            // 从用户 ID 数组中随机取出一个并赋值
+            $blank->user_id = $faker->randomElement($user_ids);
+
+            // 话题分类，同上
+            $blank->category_id = $faker->randomElement($category_ids);
         });
 
-        Blank::insert($blanks->toArray());
+        // 将数据集合转换为数组，并插入到数据库中
+        Blank::insert($choices->toArray());
     }
 
 }
