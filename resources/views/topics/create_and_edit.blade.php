c51abc7031a332b553926fe79ceb0666c80b5d03
egoeyre <egoeyre@qq.com>
2018-12-09 13:35:37 +0000

    Users can edit their own topic

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -37,7 +37,7 @@
                         <select class="form-control" name="tcategory_id" required>
                             <option value="" hidden disabled selected>请选择分类</option>
                             @foreach ($tcategories as $value)
-                                <option value="{{ $value->id }}">{{ $value->name }}</option>
+                            <option value="{{ $value->id }}" {{ $topic->tcategory_id == $value->id ? 'selected' : '' }}>{{ $value->name }}</option>
                             @endforeach
                         </select>
                     </div>
